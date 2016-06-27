aa72ee6c21729e100806ad3a6a142944bac45dcf
Dan Obermiller <dannnno16@gmail.com>
2016-06-26 19:41:30 -0500

    Setup angular2
    - Add .gitignore
    - Setup travis support
    - Add basic project structure
    - setup dependencies, compiler options, systemjs

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/// <reference path="../typings/index.d.ts" />
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './components/app';
+
+bootstrap(AppComponent);
